162bf9c367a70307feb9c2a814a681f830378841
A.Munzer <abdullah.munzer@maf.co.ae>
2020-06-10 01:44:25 +0300

    adding UIViewControllerExtenstions

---
+++ most-popular/most-popular/Extenstions/UIViewControllerExtenstions.swift
@@ -0,0 +1,31 @@
+//
+//  UIViewControllerExtenstions.swift
+//  most-popular
+//
+//  Created by Abdalla Munzer on 6/10/20.
+//  Copyright © 2020 com.most-popular. All rights reserved.
+//
+
+import UIKit
+extension UIViewController {
+    func showAlertWith(title: String , message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        UIApplication.shared.keyWindow?.rootViewController?.topMostViewController().present(alert, animated: true, completion: nil)
+    }
+
+    func topMostViewController() -> UIViewController {
+        guard let presentedVC = self.presentedViewController else {
+            return self
+        }
+        if let navigation = presentedVC as? UINavigationController {
+            return navigation.visibleViewController?.topMostViewController() ?? self
+        }
+        if let tab = presentedVC as? UITabBarController {
+            if let selectedTab = tab.selectedViewController {
+                return selectedTab.topMostViewController()
+            }
+            return tab.topMostViewController()
+        }
+        return presentedVC.topMostViewController()
+    }
+}
